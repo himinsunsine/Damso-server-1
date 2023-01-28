bc0c25b3f04dc286135a1f4d5188f6c684896d19
mir3u_ <65229910+hyoJeong0390@users.noreply.github.com>
2023-01-28 14:38:58 +0900

    Update userProvider.js

--- src/app/User/userProvider.js
+++ src/app/User/userProvider.js
@@ -3,18 +3,32 @@ const { logger } = require("../../../config/winston");
 
 const userDao = require("./userDao");
 
-exports.retrieveBookmark = async function(userid){
+exports.retrieveBookmark = async function (userid) {
+  const connection = await pool.getConnection(async (conn) => conn);
+  const result = await userDao.selectBookmark(connection, userid);
 
+  connection.release();
+  return result;
+};
+exports.retrieveUser = async function (userid) {
+  const connection = await pool.getConnection(async (conn) => conn);
+  const result = await userDao.selectUserInfo(connection, userid);
+
+  connection.release();
+  return result;
+};
+exports.retrieveFacility = async function (facilityid) {
+  const connection = await pool.getConnection(async (conn) => conn);
+  const result = await userDao.selectFacilityInfo(connection, facilityid);
+
+  connection.release();
+  return result;
+};
+
+exports.locationCheck = async function (location) {
     const connection = await pool.getConnection(async (conn) => conn);
-    const result = await userDao.selectBookmark(connection, userid);
+    const locationCheckResult = await userDao.selectFacilLocation(connection, location);
+    connection.realese();
 
-    connection.release();
-    return result;
-}
-exports.retrieveUser = async function(userid){
-    const connection = await pool.getConnection(async (conn) => conn);
-    const result = await userDao.selectUserInfo(connection, userid);
-
-    connection.release();
-    return result;
+    return locationCheckResult;
 }
