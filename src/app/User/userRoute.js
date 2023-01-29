1ed4e8f1f90171b19bce6362a48bf26417db22dd
mir3u_ <65229910+hyoJeong0390@users.noreply.github.com>
2023-01-29 20:44:53 +0900

    Update userRoute.js

--- src/app/User/userRoute.js
+++ src/app/User/userRoute.js
@@ -1,28 +1,30 @@
 module.exports = function (app) {
-    const user = require("./userController");
-  
-    // 0. 테스트 api
-    app.get("/main", user.getTest);
-  
-    //1. 즐겨찾기 조회 api
-    app.get("/main/heart/:userid", user.getBookmark);
-  
-    //2. 내 정보 조회 api
-    app.get("/main/profile/:userid", user.getUserInfo);
-  
-    //3. 내 프로필 수정 api
-    // app.post("/main/profile/:userid/image", user.postUserImage);
-  
-    //3. 내 프로필 수정 api
-    //app.post("/main/profile/:userid/nickname", user.postUserNickname);
-  
-    // 1-2. 흡연구역 간단 조회 api
-    app.get("/main/facility/simple/:facilityid", user.getFacilitySimpleInfo);
-  
-    // 1-3. 흡연구역 상세 조회 api
-    //app.get("/main/facility/detail/:facility_id", user.getFacilityDetailInfo);
+  const user = require("./userController");
 
+  // 0. 테스트 api
+  app.get("/main", user.getTest);
 
-    // 2-3. 시설 제보
-    app.post("/main/facility/register", user.postFacilityRegister);
-  };
+  //1-2. 흡연구역 간단 조회 api
+  app.get("/main/facility/simple/:facilityid", user.getFacilitySimpleInfo);
+
+  //1-3. 흡연구역 상세 조회 api
+  app.get("/main/facility/detail/:facilityid", user.getFacilityDetailInfo);
+
+  // 2-3. 시설 제보
+  app.post("/main/facility/register", user.postFacilityRegister);
+
+  //3-1. 즐겨찾기 조회 api
+  app.get("/main/heart/:userid", user.getBookmark);
+
+  //5-1. 내 정보 조회 api
+  app.get("/main/profile/:userid", user.getUserInfo);
+
+  //5-2. 내 프로필 수정 api
+  app.post("/main/profile/:userid/image", user.postUserImage);
+
+  //5-3. 내 프로필 수정 api
+  app.post("/main/profile/:userid/nickname", user.postUserNickname);
+
+  //5-4. 탈퇴 api
+  app.post("/main/profile/:userid/setting/resign", user.postUserResign);
+};
