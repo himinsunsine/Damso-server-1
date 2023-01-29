7ef49bac980ffcbe1155fcd9500ee101fb7dfb40
mir3u_ <65229910+hyoJeong0390@users.noreply.github.com>
2023-01-29 20:45:10 +0900

    Update userService.js

--- src/app/User/userService.js
+++ src/app/User/userService.js
@@ -2,16 +2,14 @@ const {logger} = require("../../../config/winston");
 const {pool} = require("../../../config/database");
 const userProvider = require("./userProvider");
 const userDao = require("./userDao");
-const baseResponse = const {logger} = require("../../../config/winston");
-const {pool} = require("../../../config/database");
-const userProvider = require("./userProvider");
-const userDao = require("./userDao");
 const baseResponse = require("../../../config/baseResponseStatus");
 const {response} = require("../../../config/response");
 const {errResponse} = require("../../../config/response");
 
 const crypto = require("crypto");
 const {connect} = require("http2");
+
+// Service : Create, Update, Delete 비즈니스 로직 처리
 
 exports.postUserImage = async function(profile, userid){
     try{
@@ -50,7 +48,22 @@ exports.postUserNickname = async function(nickname,userid,name){
         return errResponse(baseResponse.DB_ERROR);
     }
 }
+exports.postResign = async function(userid){
+    try{
+        const connection = await pool.getConnection(async (conn)=> conn);
+        const editStatus = await userDao.updateUserStatus(connection,userid);
+        
+        console.log(`${userid}의 탈퇴 처리 완료`);
+        connection.release();
 
+        return response(baseResponse.SUCCESS);
+    
+    }
+    catch(err){
+        logger.error(`App - editStatus Service error\n: ${err.message}`);
+        return errResponse(baseResponse.DB_ERROR);
+    }
+}
 
 exports.registerFacility = async function (location, title, type, installAgency, img) {
     try {
@@ -64,52 +77,11 @@ exports.registerFacility = async function (location, title, type, installAgency,
         const connection = await pool.getConnection(async (conn) => conn);
 
         const facilResult = await userDao.insertFacilInfo(connection, insertFacilInfoParams);
+        connection.release();
+        return response(baseResponse.SUCCESS);
 
     } catch (err) {
         logger.error(`App - registerFacility Service error\n: ${err.message}`);
-        return errResponse(baseResponse.NOTNULL_ERROR)
-    }
-}require("../../../config/baseResponseStatus");
-const {response} = require("../../../config/response");
-const {errResponse} = require("../../../config/response");
-
-const crypto = require("crypto");
-const {connect} = require("http2");
-
-exports.postUserImage = async function(profile, userid){
-    try{
-
-        const connection = await pool.getConnection(async (conn)=> conn);
-
-        const newparams = [profile, userid];
-        const editInfoResult = await userDao.updateUserImage(connection, newparams);
-        console.log(`${userid}의 프로필 변경 완료`);
-        connection.release();
-
-        return response(baseResponse.SUCCESS);
-    }
-    catch(err){
-        logger.error(`App - editInfo Service error\n: ${err.message}`);
-        return errResponse(baseResponse.DB_ERROR);
-    }
-}
-
-exports.postUserNickname = async function(nickname,userid,name){
-    try{
-        if(!nickname){
-            nickname = name;
-        }
-        const connection = await pool.getConnection(async (conn)=> conn);
-
-        const newparams = [nickname, userid, name];
-        const editInfoResult = await userDao.updateUserNickname(connection, newparams);
-        console.log(`${userid}의 닉네임 변경 완료`);
-        connection.release();
-
-        return response(baseResponse.SUCCESS);
-    }
-    catch(err){
-        logger.error(`App - editInfo Service error\n: ${err.message}`);
         return errResponse(baseResponse.DB_ERROR);
     }
 }
