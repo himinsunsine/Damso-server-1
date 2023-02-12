a973b595f3c4576aeba47b3e15f54b9c6ae9e894
mir3u_ <65229910+hyoJeong0390@users.noreply.github.com>
2023-02-12 19:52:30 +0900

    Update userController.js

--- src/app/User/userController.js
+++ src/app/User/userController.js
@@ -28,31 +28,7 @@ exports.getBookmark = async function (req, res) {
   const userBookmark = await userProvider.retrieveBookmark(userid);
   return res.send(response(baseResponse.SUCCESS, userBookmark));
 };
-/**
- * API No. 1-2
- * API Name : 내 정보 조회 API
- * [GET] /main/profile/{user_id}
- */
-exports.getUserInfo = async function (req, res) {
-  const userid = req.params.userid;
 
-  const userInfo = await userProvider.retrieveUser(userid);
-  return res.send(response(baseResponse.SUCCESS, userInfo));
-};
-
-/**
- * API No. 1-3
- * API Name : 내 이미지 수정 API
- * [POST] /main/profile/{user_id}/image
- */
-exports.postUserImage = async function (req, res) {
-  const profile = req.body.profile;
-  const userid = req.params.userid;
-
-  const UserImageResponse = await userService.postUserImage(profile, userid);
-
-  return res.send(UserImageResponse);
-};
 
 /**
  * API No. 2-3
@@ -81,6 +57,17 @@ exports.postUserImage = async function (req, res) {
   return res.send(registerResponse);
 };
 
+/**
+ * API No. 5-1
+ * API Name : 내 정보 조회 API
+ * [GET] /main/profile/{user_id}
+ */
+exports.getUserInfo = async function (req, res) {
+  const userid = req.params.userid;
+
+  const userInfo = await userProvider.retrieveUser(userid);
+  return res.send(response(baseResponse.SUCCESS, userInfo));
+};
 
 /**
  * API No. 5-3
@@ -100,8 +87,9 @@ exports.postUserNickname = async function (req, res) {
   return res.send(UserNicknameResponse);
 };
 
+
 /**
- * API No. 5-4
+ * API No. 5-2
  * API Name : 탈퇴 API
  * [POST] /main/profile/:userid/setting/resign
  */
@@ -112,16 +100,19 @@ exports.postUserResign = async function (req, res) {
   return res.send(UserResignResponse);
 };
 
+
 /**
  * API No. 5-4
- * API Name : 탈퇴 API
- * [POST] /main/profile/:userid/setting/resign
+ * API Name : 내 이미지 수정 API
+ * [POST] /main/profile/{user_id}/image
  */
-exports.postUserResign = async function (req, res) {
+exports.postUserImage = async function (req, res) {
+  const profile = req.body.profile;
   const userid = req.params.userid;
 
-  const UserResignResponse = await userService.postResign(userid);
-  return res.send(UserResignResponse);
+  const UserImageResponse = await userService.postUserImage(profile, userid);
+
+  return res.send(UserImageResponse);
 };
 
 /**
@@ -148,30 +139,3 @@ exports.getFacilityDetailInfo = async function (req, res) {
   return res.send(response(baseResponse.SUCCESS, facilityInfo));
 };
 
-
-/**
- * API No. 2-3
- * API Name : 시설 제보
- * [POST] /main/facility/register
- */  
-exports.postFacilityRegister = async function (req, res) {
-  /**
-   * Body; location, title, type, installAgency, img
-   */
-  const {location, title, type, installAgency, img} = req.body;
-
-  // 빈 값 체크
-  if (!location)
-      return res.send(response(baseResponse.REGISTER_LOCATION_EMPTY));
-  
-
-  const registerResponse = await userService.registerFacility(
-      location,
-      title,
-      type,
-      installAgency,
-      img
-    );
- 
-  return res.send(registerResponse);
-  };
